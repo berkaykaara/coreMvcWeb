7a528ff57e0148ec73be14836bd9e3b9b66a7935
berkaykaara <berkay.kara02@bilgiedu.net>
2024-09-14 03:03:16 +0300

    coreMvcWebApplication

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using coreMvcWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace coreMvcWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
